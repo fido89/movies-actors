e6f15f16077fa703ab81c6334f4cce71db4e7412
Denis <d.fidersek@gmail.com>
2019-12-02 21:55:09 +0100

    Added flyway integration for DB migration

---
+++ src/main/java/db/FlywayIntegrator.java
@@ -0,0 +1,34 @@
+package db;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
+import org.hibernate.boot.Metadata;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.integrator.spi.Integrator;
+import org.hibernate.service.spi.SessionFactoryServiceRegistry;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FlywayIntegrator implements Integrator {
+    public static final Logger logger = Logger.getLogger("FlywayIntegrator");
+
+    @Override
+    public void integrate(Metadata metadata, SessionFactoryImplementor sessionFactoryImplementor, SessionFactoryServiceRegistry sessionFactoryServiceRegistry) {
+        logger.log(Level.INFO, "Starting Flyway Migration");
+        Flyway flyway = Flyway.configure().dataSource("jdbc:h2:file:./data/demo", "", "").load();
+        try {
+            flyway.migrate();
+        } catch (FlywayException e) {
+            logger.log(Level.SEVERE, "Error while migrating:", e);
+        }
+        logger.log(Level.INFO, "Finished Flyway Migration");
+    }
+
+    @Override
+    public void disintegrate(SessionFactoryImplementor sessionFactoryImplementor, SessionFactoryServiceRegistry sessionFactoryServiceRegistry) {
+
+    }
+}
